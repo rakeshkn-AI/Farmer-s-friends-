d227d15c7c93090b47bbe48c73fbf2c1a0906e7e
Rakesh kn <68599502+rakeshkn-AI@users.noreply.github.com>
2022-06-01 01:36:38 +0530

    Add files via upload

---
+++ cultivation_machines.php
@@ -0,0 +1,82 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(isset($_GET['action']) && $_GET['action']=="add"){
+	$id=intval($_GET['id']);
+	if(isset($_SESSION['cart'][$id])){
+		$_SESSION['cart'][$id]['quantity']++;
+	}else{
+		$sql_p="SELECT * FROM products WHERE id={$id}";
+		$query_p=mysql_query($sql_p);
+		if(mysql_num_rows($query_p)!=0){
+			$row_p=mysql_fetch_array($query_p);
+			$_SESSION['cart'][$row_p['id']]=array("quantity" => 1, "price" => $row_p['productPrice']);
+			header('location:index.php');
+		}else{
+			$message="Product ID is invalid";
+		}
+	}
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Meta -->
+		<meta charset="utf-8">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
+	    <meta name="keywords" content="MediaCenter, Template, eCommerce">
+	    <meta name="robots" content="all">
+<aline
+	    <title>Farmers Friend|| Home </title>
+		  <p id="slogan"> By Rakesh</p>
+		
+
+	    <!-- Bootstrap Core CSS -->
+	    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	    
+	    <!-- Customizable CSS -->
+	    <link rel="stylesheet" href="assets/css/main.css">
+	    <link rel="stylesheet" href="assets/css/green.css">
+	    <link rel="stylesheet" href="assets/css/owl.carousel.css">
+		<link rel="stylesheet" href="assets/css/owl.transitions.css">
+		<!--<link rel="stylesheet" href="assets/css/owl.theme.css">-->
+		<link href="assets/css/lightbox.css" rel="stylesheet">
+		<link rel="stylesheet" href="assets/css/animate.min.css">
+		<link rel="stylesheet" href="assets/css/rateit.css">
+		<link rel="stylesheet" href="assets/css/bootstrap-select.min.css">
+
+		<!-- Demo Purpose Only. Should be removed in production -->
+		
+		
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="assets/images/favicon.ico">
+
+	</head>
+    <body class="cnt-home">
+	
+		
+	
+		<!-- ============================================== HEADER ============================================== -->
+<header class="header-style-1">
+<?php include('includes/menu-bar.php');?>
+	<?php include('includes/top-header.php');?>
+
+</header>
+
+<body>
+<img src="images/m1.jpg" height="148" width="254" />&nbsp;
+<img src="images/m2.jpg" height="148" width="254" />&nbsp;
+<img src="images/m3.jpg" height="148" width="254" />&nbsp;
+<img src="images/m4.jpg" height="148" width="254" />&nbsp;
+<img src="images/m5.jpg" height="148" width="254" />&nbsp;
+<img src="images/m6.jpg" height="148" width="254" />&nbsp;
+<img src="images/m7.jpg" height="148" width="254" />&nbsp;
+<img src="images/m8.jpg" height="148" width="254" />&nbsp;
+</body>
+</html>
